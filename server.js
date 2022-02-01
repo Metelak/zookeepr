fa9d01603fcdbea225ea36161ad296ccdf0a978c
coyote <megan.metelak@gmail.com>
2022-02-01 16:07:32 -0600

    functioning GET and POST requests

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+// import and use fs libary to write data to animals.json
+const fs = require('fs');
+// another module built into node.js that works with file and directory paths
+const path = require('path');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -46,7 +56,36 @@ function findById(id, animalsArray) {
     return result;
 }
 
-// route to filter
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    // push - adds a new animal to the array
+    animalsArray.push(animal);
+    // write to the animals.json file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        // convert JavaScript array data to save as JSON
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+// GET route to filter
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -55,7 +94,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-// route to filter through array and return a single object
+// GET route to filter through array and return a single object
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     // respond with result of findById, else present with 404 error if no record exists    
@@ -63,6 +102,22 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result);
     } else {
         res.send(404);
+    }
+});
+
+// POST route for client requesting server to accept data
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 });
 
