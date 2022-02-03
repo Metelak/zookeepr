011d9f69f8d40709424e4df55bbc577110f77258
coyote <megan.metelak@gmail.com>
2022-02-03 14:01:15 -0600

    playing around with paths

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const animalRoutes = require('./apiRoutes/animalRoutes');
+const animalRoutes = require('../apiRoutes/animalRoutes');
 
 router.use(animalRoutes);
 
